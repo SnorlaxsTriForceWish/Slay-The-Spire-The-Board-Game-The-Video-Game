3b7d0ddd78473c2fcc30f9b9c835d287349cf209
degraffa <ard96@cornell.edu>
2025-11-23 01:18:31 -0500

    Guides

---
+++ src/main/java/sts_tbg_tvg/characters/BoardGameIronclad.java
@@ -0,0 +1,269 @@
+package sts_tbg_tvg.characters;
+
+import basemod.BaseMod;
+import basemod.abstracts.CustomPlayer;
+import basemod.animations.AbstractAnimation;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.esotericsoftware.spine.AnimationState;
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.EnergyManager;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.cutscenes.CutscenePanel;
+import com.megacrit.cardcrawl.events.city.Vampires;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
+import com.megacrit.cardcrawl.localization.CharacterStrings;
+import com.megacrit.cardcrawl.screens.CharSelectInfo;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import sts_tbg_tvg.StsTbgTvgMod;
+
+import java.util.ArrayList;
+
+import static sts_tbg_tvg.StsTbgTvgMod.characterPath;
+import static sts_tbg_tvg.StsTbgTvgMod.makeID;
+
+public class BoardGameIronclad extends CustomPlayer {
+    // Character ID and Card Color - must match
+    public static class Enums {
+        @SpireEnum
+        public static AbstractPlayer.PlayerClass BOARD_GAME_IRONCLAD;
+        @SpireEnum(name = "BOARD_GAME_RED")
+        public static AbstractCard.CardColor BOARD_GAME_RED;
+    }
+
+    // Character-specific static values
+    private static final int ENERGY_PER_TURN = 3;
+    private static final int MAX_HP = 70;
+    private static final int STARTING_GOLD = 99;
+    private static final int CARD_DRAW = 5;
+    private static final int ORB_SLOTS = 0;
+
+    // Localization strings
+    private static final String ID = makeID("BoardGameIronclad");
+    private static final CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString(ID);
+    private static final String[] NAMES = characterStrings.NAMES;
+    private static final String[] TEXT = characterStrings.TEXT;
+
+    // Image paths - we'll use base game Ironclad assets
+    private static final String SHOULDER_1 = "images/characters/ironclad/shoulder.png";
+    private static final String SHOULDER_2 = "images/characters/ironclad/shoulder2.png";
+    private static final String CORPSE = "images/characters/ironclad/corpse.png";
+
+    // Character select screen assets
+    private static final String BUTTON = "images/ui/charSelect/ironcladButton.png";
+    private static final String PORTRAIT = "images/ui/charSelect/ironcladPortrait.jpg";
+
+    // Card backgrounds - we'll use the base red color card backgrounds
+    private static final String BG_ATTACK_512 = "images/512/bg_attack_red.png";
+    private static final String BG_SKILL_512 = "images/512/bg_skill_red.png";
+    private static final String BG_POWER_512 = "images/512/bg_power_red.png";
+    private static final String BG_ENERGY_512 = "images/512/bg_energy_red.png";
+    private static final String BG_ATTACK_1024 = "images/1024/bg_attack_red.png";
+    private static final String BG_SKILL_1024 = "images/1024/bg_skill_red.png";
+    private static final String BG_POWER_1024 = "images/1024/bg_power_red.png";
+    private static final String BG_ENERGY_1024 = "images/1024/bg_energy_red.png";
+
+    // Energy orb assets
+    private static final String ORB_VFX = "images/ui/topPanel/energyOrbRed.png";
+
+    public BoardGameIronclad(String name) {
+        super(name, Enums.BOARD_GAME_IRONCLAD, null, null, null, null);
+
+        // Load energy orb
+        this.dialogX = (this.drawX + 0.0F * Settings.scale);
+        this.dialogY = (this.drawY + 220.0F * Settings.scale);
+
+        // Initialize starting stats
+        initializeClass(
+                CORPSE,
+                SHOULDER_1, SHOULDER_2,
+                CORPSE,
+                getLoadout(),
+                0.0F, 0.0F, 200.0F, 250.0F,
+                new EnergyManager(ENERGY_PER_TURN)
+        );
+
+        // Load animations - using base game Ironclad skeleton
+        loadAnimation("images/characters/ironclad/idle/skeleton.atlas",
+                "images/characters/ironclad/idle/skeleton.json",
+                1.0f);
+
+        AnimationState.TrackEntry e = this.state.setAnimation(0, "Idle", true);
+        e.setTime(e.getEndTime() * MathUtils.random());
+    }
+
+    @Override
+    public ArrayList<String> getStartingDeck() {
+        ArrayList<String> retVal = new ArrayList<>();
+
+        // Standard Ironclad starting deck
+        // 5 Strikes
+        for (int i = 0; i < 5; i++) {
+            retVal.add("Strike_R");
+        }
+        // 4 Defends
+        for (int i = 0; i < 4; i++) {
+            retVal.add("Defend_R");
+        }
+        // 1 Bash
+        retVal.add("Bash");
+
+        return retVal;
+    }
+
+    @Override
+    public ArrayList<String> getStartingRelics() {
+        ArrayList<String> retVal = new ArrayList<>();
+        // We'll create a custom starting relic
+        retVal.add(makeID("BoardGameStarterRelic"));
+        UnlockTracker.markRelicAsSeen(makeID("BoardGameStarterRelic"));
+        return retVal;
+    }
+
+    @Override
+    public CharSelectInfo getLoadout() {
+        return new CharSelectInfo(
+                NAMES[0], // Character name
+                TEXT[0],  // Character description
+                MAX_HP,   // Starting HP
+                MAX_HP,   // Max HP
+                ORB_SLOTS, // Orb slots
+                STARTING_GOLD, // Starting gold
+                CARD_DRAW, // Card draw per turn
+                this,
+                getStartingRelics(),
+                getStartingDeck(),
+                false
+        );
+    }
+
+    @Override
+    public String getTitle(PlayerClass playerClass) {
+        return NAMES[1];
+    }
+
+    @Override
+    public AbstractCard.CardColor getCardColor() {
+        return Enums.BOARD_GAME_RED;
+    }
+
+    @Override
+    public Color getCardRenderColor() {
+        return Color.SCARLET;
+    }
+
+    @Override
+    public String getAchievementKey() {
+        return "BOARD_GAME_IRONCLAD";
+    }
+
+    @Override
+    public ArrayList<AbstractCard> getCardPool(ArrayList<AbstractCard> tmpPool) {
+        // For now, we'll return empty - eventually we'll add custom cards
+        // Or we could include base Ironclad cards
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Color getCardTrailColor() {
+        return Color.RED.cpy();
+    }
+
+    @Override
+    public String getLeaderboardCharacterName() {
+        return "BOARD_GAME_IRONCLAD";
+    }
+
+    @Override
+    public Texture getEnergyImage() {
+        return ImageMaster.RED_ORB_FLASH_VFX;
+    }
+
+    @Override
+    public int getAscensionMaxHPLoss() {
+        return 4;
+    }
+
+    @Override
+    public BitmapFont getEnergyNumFont() {
+        return FontHelper.energyNumFontRed;
+    }
+
+    @Override
+    public void doCharSelectScreenSelectEffect() {
+        CardCrawlGame.sound.playA("ATTACK_HEAVY", MathUtils.random(-0.2F, 0.2F));
+        CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.SHORT, true);
+    }
+
+    @Override
+    public String getCustomModeCharacterButtonSoundKey() {
+        return "ATTACK_HEAVY";
+    }
+
+    @Override
+    public String getLocalizedCharacterName() {
+        return NAMES[0];
+    }
+
+    @Override
+    public AbstractPlayer newInstance() {
+        return new BoardGameIronclad(name);
+    }
+
+    @Override
+    public String getSpireHeartText() {
+        return TEXT[1];
+    }
+
+    @Override
+    public Color getSlashAttackColor() {
+        return Color.RED;
+    }
+
+    @Override
+    public AbstractGameAction.AttackEffect[] getSpireHeartSlashEffect() {
+        return new AbstractGameAction.AttackEffect[]{
+                AbstractGameAction.AttackEffect.FIRE,
+                AbstractGameAction.AttackEffect.BLUNT_HEAVY,
+                AbstractGameAction.AttackEffect.FIRE
+        };
+    }
+
+    @Override
+    public String getVampireText() {
+        return TEXT[2];
+    }
+
+    // Meta class for registration
+    public static class Meta {
+        public static void registerColor() {
+            BaseMod.addColor(
+                    Enums.BOARD_GAME_RED,
+                    Color.RED,
+                    BG_ATTACK_512, BG_SKILL_512, BG_POWER_512, BG_ENERGY_512,
+                    BG_ATTACK_1024, BG_SKILL_1024, BG_POWER_1024, BG_ENERGY_1024,
+                    ORB_VFX,
+                    BUTTON,
+                    PORTRAIT
+            );
+        }
+
+        public static void registerCharacter() {
+            BaseMod.addCharacter(
+                    new BoardGameIronclad(NAMES[0]),
+                    BUTTON,
+                    PORTRAIT,
+                    Enums.BOARD_GAME_IRONCLAD
+            );
+        }
+    }
+}
---
+++ src/main/java/sts_tbg_tvg/relics/BoardGameStarterRelic.java
@@ -0,0 +1,34 @@
+package sts_tbg_tvg.relics;
+
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import sts_tbg_tvg.characters.BoardGameIronclad;
+
+import static sts_tbg_tvg.StsTbgTvgMod.makeID;
+
+public class BoardGameStarterRelic extends BaseRelic {
+    private static final String NAME = "BoardGameStarterRelic";
+    public static final String ID = makeID(NAME);
+
+    // Use Burning Blood image from base game
+    private static final String IMG_NAME = "burningBlood.png";
+
+    private static final int HEAL_AMOUNT = 6;
+
+    public BoardGameStarterRelic() {
+        super(ID, IMG_NAME, BoardGameIronclad.Enums.BOARD_GAME_RED, RelicTier.STARTER, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public void onVictory() {
+        this.flash();
+        this.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        AbstractDungeon.player.heal(HEAL_AMOUNT);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+}
